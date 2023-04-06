9986ac7953fa11bd98afbc892b10bb6090fdaabe
U-keun Song <sukeun319@gmail.com>
2023-04-06 12:38:36 +0900

    minor revision

--- baseball_ukeun.java
+++ baseball_ukeun.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class Main{
+public class baseball_ukeun{
     public static void main(String[] args) {
         int[] numbers = {0,1,2,3,4,5,6,7,8,9};
         int[] answer = new int[3];
